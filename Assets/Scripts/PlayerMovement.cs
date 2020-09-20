e2981329d15994ea03c8063f7379d19f8b4be184
gilzoide <gilzoide@gmail.com>
2020-09-20 12:21:20 -0300

    Add placeholder color rectangle sprite, add moving Player inside Train

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 4f;
+    public Rigidbody2D rigidBody;
+
+    void Awake()
+    {
+        if (!rigidBody)
+        {
+            rigidBody = GetComponent<Rigidbody2D>();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 movingTo = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rigidBody.velocity = movingTo * speed;
+    }
+}
