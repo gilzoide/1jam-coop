818a5684d8c632ccbb7bf1b1a923151764e0f4bd
Gustavo Fournier <gustavo.s.fournier@gmail.com>
2020-09-27 02:14:19 -0300

    Add Level Manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public Transform train;
+    public GameObject[] levels;
+
+    private int currentLevel;
+
+    void Awake()
+    {
+        NextLevel();
+    }
+
+    void NextLevel()
+    {
+        GameObject level = Instantiate(levels[currentLevel]);
+        level.GetComponent<WaveManager>().train = train;
+        level.GetComponent<WaveManager>().endLevel.AddListener(EndLevel);
+    }
+
+    void EndLevel()
+    {
+        if (currentLevel < levels.Length - 1)
+        {
+            currentLevel++;
+            NextLevel();
+        }
+    }
+}
---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WaveManager : MonoBehaviour
+{
+    public Transform train;
+    public WeaponInfo weaponInfo;
+    public UnityEvent endLevel;
+    [System.Serializable]
+    public class Waves { public GameObject[] enemies; }
+    public Waves[] waves;
+
+    private int enemies;
+    private int wave;
+
+    void Awake()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        yield return new WaitForSeconds(3);
+
+        enemies = waves[wave].enemies.Length;
+
+        foreach (GameObject enemyPrefab in waves[wave].enemies)
+        {
+            Vector2 spawnPosition = new Vector2(
+                Random.Range(0, 2) == 0 ? 10 : -10,
+                Random.Range(-5f, 5f)
+            );
+
+            GameObject enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+            GameObject crosshair = enemy.transform.Find("Crosshair").gameObject;
+            enemy.GetComponent<GetNear>().target = train;
+            enemy.GetComponent<GetNear>().onNear.AddListener(() => crosshair.GetComponent<Crosshair>().LookAt(train));
+            enemy.GetComponent<GetNear>().onNear.AddListener(() => enemy.GetComponent<RepeatedShooter>().Shoot(weaponInfo));
+            enemy.GetComponent<EnergyHolder>().onEnergyEmpty.AddListener(EnemyDied);
+        }
+    }
+
+    void EnemyDied()
+    {
+        enemies--;
+
+        if (enemies <= 0)
+        {
+            if (wave < waves.Length - 1)
+            {
+                wave++;
+                StartCoroutine(SpawnEnemies());
+            }
+            else
+            {
+                endLevel.Invoke();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
