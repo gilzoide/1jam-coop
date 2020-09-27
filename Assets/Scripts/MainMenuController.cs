9250906b1400517827f5385238b9b7002216d2a2
gilzoide <gilzoide@gmail.com>
2020-09-27 14:14:02 -0300

    WIP Add MainMenu scene with about panel and start button that loads some scene

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public ScoreInfo gameplayScoreInfo;
+    public Object gameplayScene;
+
+    public void StartGameplay()
+    {
+        gameplayScoreInfo.Reset();
+        SceneManager.LoadScene(gameplayScene.name);
+    }
+}
