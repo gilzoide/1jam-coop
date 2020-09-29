73d80ea7321689fbcbee38abbaa889aff5417137
gilzoide <gilzoide@gmail.com>
2020-09-29 14:53:26 -0300

    Move UI related scripts/prefabs into their own folder, add GameOver panel prefab and FadeAlpha component

---
+++ Assets/Scripts/GameOverController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverController : MonoBehaviour
+{
+    public FadeAlpha fadeAlpha;
+    public Button continueButton;
+
+    void Awake()
+    {
+        if (!fadeAlpha)
+        {
+            fadeAlpha = GetComponentInChildren<FadeAlpha>();
+        }
+    }
+
+    public void Appear()
+    {
+        Time.timeScale = 0f;
+        fadeAlpha.FadeIn();
+        EventSystem.current.SetSelectedGameObject(continueButton.gameObject);
+    }
+
+    public void BackToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/UI/FadeAlpha.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeAlpha : MonoBehaviour
+{
+    public CanvasGroup canvasGroup;
+    public float fadeDuration;
+
+    void Awake()
+    {
+        if (!canvasGroup)
+        {
+            canvasGroup = GetComponentInChildren<CanvasGroup>();
+        }
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(FadeToInSeconds(1f, fadeDuration));
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeToInSeconds(1f, fadeDuration));
+    }
+
+    private IEnumerator FadeToInSeconds(float targetAlpha, float seconds)
+    {
+        var previousAlpha = canvasGroup.alpha;
+        var timePast = 0f;
+
+        while (timePast < seconds)
+        {
+            timePast += Time.unscaledDeltaTime;
+            canvasGroup.alpha = Mathf.Lerp(previousAlpha, targetAlpha, timePast / seconds);
+            yield return null;
+        }
+    }
+}
