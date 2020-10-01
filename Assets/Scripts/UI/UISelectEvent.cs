5584f3ee0ba72db62f2f2aff488c04873c94a518
gilzoide <gilzoide@gmail.com>
2020-10-01 15:55:39 -0300

    Add UI Button click SFX, add main menu music

---
+++ Assets/Scripts/UI/UISelectEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class UISelectEvent : MonoBehaviour, ISelectHandler
+{
+    public UnityEvent selectHandler;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        selectHandler.Invoke();
+    }
+}
