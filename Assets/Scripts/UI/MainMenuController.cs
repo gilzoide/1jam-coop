c9c2461a93ddd8a9c7558290d7200d8be8b3f4bd
Gustavo Fournier <gustavo.s.fournier@gmail.com>
2020-10-01 00:35:01 -0300

    Update build config

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -1,16 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
     public ScoreInfo gameplayScoreInfo;
-    public Object gameplayScene;
 
     public void StartGameplay()
     {
         gameplayScoreInfo.Reset();
-        SceneManager.LoadScene(gameplayScene.name);
+        SceneManager.LoadScene("Gameplay");
     }
 }
