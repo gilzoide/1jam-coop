d13f74f53945d32cc9846ced889c7eed9aba8bd9
Gustavo Fournier <gustavo.s.fournier@gmail.com>
2020-10-01 23:38:17 -0300

    Add background movement

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public float speed;
+
+    private Vector2 startPos;
+
+    void Awake()
+    {
+        startPos = new Vector2(0, -9.15f);
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.down * Time.deltaTime * speed;
+
+        if (transform.position.y < -37.45f)
+        {
+            transform.position = startPos;
+        }
+    }
+}
